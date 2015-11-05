ee66b2815e491fb5676c2a0d0e21afbca5e6e43b
Morgan Evans <evansmorg@gmail.com>
2015-11-04 22:29:18 -0800

    fixed interface

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -9,38 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    let topics = [
-        ["Subject" : "Mathematics", "Description" : "Mathematics Quiz!"],
-        ["Subject" : "Marvel Super Heroes", "Description" : "Marvel Super Heroes Quiz!"],
-        ["Subject" : "Science", "Description" : "Science Quiz!"]
-    ]
+    
+    private let subjects = [
+        "Mathematics", "Marvel Super Heroes", "Science"]
     
     let cellTableIdentifier = "CellTableIdentifier"
-    
-    
-    func tableView(tableView: UITableView,
-        numberOfRowsInSection section: Int) -> Int {
-            return topics.count
-    }
-
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(cellTableIdentifier, forIndexPath: indexPath) as! QuizCellTableViewCell
-        
-        let imageView = UIImageView(frame: CGRectMake(10, 10, 5, 5))
-        let image = UIImage(named: "quiz")
-        imageView.image = image
-        cell.imageView?.image = image
-        let rowData = topics[indexPath.row]
-        cell.subject = rowData["Subject"]!
-        cell.desc = rowData["Description"]!
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 50
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,6 +24,65 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func settings(sender: UIBarButtonItem) {
+        let title = "Settings"
+        let controller = UIAlertController(title: title, message: nil,
+            preferredStyle: .Alert)
+        let action = UIAlertAction(title: "Sounds good",
+            style: .Default, handler: nil)
+        controller.addAction(action)
+        
+        presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return subjects.count
+    }
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            var cell = tableView.dequeueReusableCellWithIdentifier(cellTableIdentifier)
+                as UITableViewCell!
+            if (cell == nil) {
+                cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: cellTableIdentifier)
+            }
+            
+            let image = UIImage(named: "quiz_img")
+            cell!.imageView?.image = image
+            let highlightedImage = UIImage(named: "quiz_img2")
+            cell!.imageView?.highlightedImage = highlightedImage
+            
+            cell!.textLabel?.text = subjects[indexPath.row]
+            cell!.textLabel?.font = UIFont .boldSystemFontOfSize(18)
+            cell!.detailTextLabel?.text = "Quizzes quizzes are so fun, quizzes are for everyone!"
+            cell!.detailTextLabel?.font = UIFont.systemFontOfSize(12)
+            return cell!
+    }
+    
+    
+    func tableView(tableView: UITableView,
+        didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+            let rowValue = subjects[indexPath.row]
+            let message = "You picked \(rowValue)"
+            
+            let controller = UIAlertController(title: "OK",
+                message: message, preferredStyle: .Alert)
+            let action = UIAlertAction(title: "Yes",
+                style: .Default, handler: nil)
+            controller.addAction(action)
+            
+            presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    func tableView(tableView: UITableView,
+        heightForRowAtIndexPath indexPath: NSIndexPath)
+        -> CGFloat {
+            return indexPath.row == 0 ? 120 : 70
+    }
+
 
 
 }
